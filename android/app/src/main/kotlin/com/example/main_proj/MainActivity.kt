df2d9bd620ffaa6f53d5486150883a68ff25dba8
Krittapat <krittapat.pra@bigc.co.th>
2023-10-06 17:46:54 +0700

    get set auth

---
+++ android/app/src/main/kotlin/com/example/main_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.main_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/module_1.dart
+++ lib/module_1.dart
@@ -1,8 +1,20 @@
 library module_1;
+import 'package:auth_module/auth.dart';
 
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
   double mul(double x, double y) => x * y;
+}
+
+
+class Auth {
+  void setAuth(String value) {
+    FileSystemManager().token = value;
+  }
+
+  String getAuth() {
+    return FileSystemManager().token ?? "Token is null";
+  }
 }
