3cf378f0f622b1739b559ffeffddb40a8b3a244f
Krittapat <krittapat.pra@bigc.co.th>
2023-10-06 18:22:08 +0700

    get set auth

--- lib/module_1.dart
+++ lib/module_1.dart
@@ -1,5 +1,7 @@
 library module_1;
 import 'package:auth_module/auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 /// A Calculator.
 class Calculator {
@@ -16,5 +18,14 @@ class Auth {
 
   String getAuth() {
     return FileSystemManager().token ?? "Token is null";
+  }
+}
+
+class Content {
+  Widget buildBtn () {
+    return TextButton(
+      onPressed: () => throw Exception(),
+      child: const Text("Throw Test Exception"),
+    );
   }
 }
